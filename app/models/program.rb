a1d43421d6d4ec0204bba7882fc7fd97f69d04ed
Maxim <jesus.dead.drunk@gmail.com>
2020-02-28 12:53:16 +0300

    [dev] add sort for program scope search by term

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,8 +1,17 @@
 class Program < ApplicationRecord
   has_many :programs_users
   has_many :users, through: :programs_users
+  validates :title, presence: true
+  validates :description, presence: true
 
-  scope :by_term, ->(term) { where('title LIKE ?', "%#{term}%") }
+
+  scope :by_term, ->(term) do 
+    joins(:programs_users).
+    select(:id, :title, :description, "COUNT(`user_id`) as users_subscribed").
+    where('title LIKE ?', "%#{term}%").
+    group(:id).
+    order(users_subscribed: :desc)
+  end
 
   def self.blueprint_options
     {
