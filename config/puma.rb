8de90de92a4ffe4c5e5a383bdb2e789811fa65ea
Maxim <jesus.dead.drunk@gmail.com>
2020-02-28 09:50:02 +0300

    [dev] change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,9 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-bind 'tcp://0.0.0.0:3000'
+bind 'tcp://0.0.0.0:3001'
 environment ENV.fetch('RAILS_ENV') { 'development' }
-
-pidfile 'tmp/pids/server.pid'
-state_path 'tmp/pids/puma.state'
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
